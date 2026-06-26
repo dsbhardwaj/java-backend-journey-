3af5c2de1e0273935ee5377e13c80a2fbe962c38
drishti <drishtiibhardwaj@gmail.com>
2026-06-26 13:56:58 +0530

    classes objects and methods

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args) {
+        user user1 = new user();
+        user1.name = "drishti";
+        user1.id = 1;
+        user1.email = "drishti@gmail.com";
+        user1.score = 100;   
+    
+    user1.login();
+    }
+}
---
+++ user.java
@@ -0,0 +1,10 @@
+public class user{
+    int id;
+    String name;
+    String email;
+    int score;
+
+public void login(){
+     System.out.println( name + " logged In");
+}
+}
